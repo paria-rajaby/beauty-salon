1551b60256f7c57289d19f1e00bf326fe786fbc4
Paria Rajaby <pariarajaby2004@gmail.com>
2025-12-26 16:53:32 +0330

    add sign in and sign up pages

---
+++ js/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://vyrgkkuzadefqirmzrej.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5cmdra3V6YWRlZnFpcm16cmVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYyMTUyMTUsImV4cCI6MjA4MTc5MTIxNX0.DM6irl8qtwI_UVTB6EPx2YKbg7hP5S_xPG9EumX_QLM"
+);
